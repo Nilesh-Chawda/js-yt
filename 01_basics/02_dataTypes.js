ab446403a8a6feb885c93820d9d46145e5f3d54a
Nilesh Chawda <138156845+Nilesh-Chawda@users.noreply.github.com>
2023-08-27 14:11:50 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) // We are using nodejs, not browser
+
+//console.log(3+ 
+//    3 )  // Code readable should be high
+
+//  DATA TYPES
+
+let name = "Nilesh"
+let age = 18
+let isloggedIn = true
+
+/* Pre-Defined */
+// Number=> 2 to the power 53
+// bigint
+//string = ""
+//boolean => true/false
+//null
+//undefined
+//symbol => unique
+
+/* User-Defined*/
+
+//Object
+
+console.log(typeof null )   // object
+console.log(typeof undefined ) //undefined
