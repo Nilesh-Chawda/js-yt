62c3fac0c66116f268cbcbca19368a5e3fd19591
Nilesh Chawda <138156845+Nilesh-Chawda@users.noreply.github.com>
2023-08-27 15:00:19 +0000

    Data Type Conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score));
+
+var valueinNumber = Number(score)
+console.log(typeof (valueinNumber))
+console.log(valueinNumber)
+
+/* Numbers */ 
+// "33" => 33
+// "123abc" => Nan
+// true => 1 , false => 0
+
+/* Boolean */
+// 1 => true; 0=> false
+// ""=> false
+// "Hitesh" => true 
+
